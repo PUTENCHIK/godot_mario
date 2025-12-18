9515bac9d6b873f55f86ae0be5112d814fdd477d
Maxim Olifirenko <opaolifirenkomaxim@gmail.com>
2025-12-18 18:03:06 +0800

    Added choice of font into settings

--- autoload/settings.gd
+++ autoload/settings.gd
@@ -1,3 +1,15 @@
 extends Node
 
+@onready var main_theme: Theme = preload("res://resources/main_theme.tres")
+@onready var fonts: Dictionary = {
+	"Pixelify": preload("res://resources/pixelify-font.ttf"),
+	"Tomorrow": preload("res://resources/tomorrow-font.ttf"),
+}
+
 signal show_fps(toggle_on)
+
+func update_font(font_name: String):
+	if font_name not in fonts:
+		print("[ERROR] No such font '%s' in files" % [font_name])
+		pass
+	main_theme.default_font = fonts[font_name]
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,9 +1,11 @@
 extends Node
 
 @onready var content: Panel = $Panel
+@onready var font_option: OptionButton = $Panel/CenterContainer/MarginContainer/Main/Settings/VBoxContainer/FontsOptionButton
 
 func _ready() -> void:
 	content.visible = false
+	_set_fonts_to_option_button()
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("menu"):
@@ -25,3 +27,12 @@ func _exit_game():
 
 func _on_check_button_toggled(toggled_on: bool) -> void:
 	Settings.show_fps.emit(toggled_on)
+
+func _set_fonts_to_option_button():
+	var fonts: Array = Settings.fonts.keys()
+	for font in fonts:
+		font_option.add_item(font)
+	font_option.selected = 0
+
+func _on_font_changed(index: int) -> void:
+	Settings.update_font(font_option.get_item_text(index))
